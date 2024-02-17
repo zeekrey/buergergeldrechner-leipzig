abdb63fe18ffa26ce6a55dce45ac31074ee3934c
zeekrey <christian@krey.io>
2024-02-17 23:03:47 +0100

    fix linter issues

--- src/components/ui/radio-group.jsx
+++ src/components/ui/radio-group.jsx
@@ -1,10 +1,9 @@
 "use client"
 
-import * as React from "react"
+import { cn } from "@/lib/utils"
 import * as RadioGroupPrimitive from "@radix-ui/react-radio-group"
 import { Circle } from "lucide-react"
-
-import { cn } from "@/lib/utils"
+import * as React from "react"
 
 const RadioGroup = React.forwardRef(({ className, ...props }, ref) => {
   return (<RadioGroupPrimitive.Root className={cn("grid gap-2", className)} {...props} ref={ref} />);
@@ -14,11 +13,11 @@ RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
 const RadioGroupItem = React.forwardRef(({ className, ...props }, ref) => {
   return (
     (<RadioGroupPrimitive.Item
-      ref={ref}
       className={cn(
         "aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
+      ref={ref}
       {...props}>
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
         <Circle className="h-2.5 w-2.5 fill-current text-current" />
